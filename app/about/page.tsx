9fd18b84169ae63a2e2768df117f5f8e7f9fc997
Rugved Joshi <rugvedsamruddhi@gmail.com>
2025-05-19 20:38:26 +0530

    about me page to display about myself

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function About() {
+  return (
+    <section className="section container">
+      <h2>About Me</h2>
+      <p>
+        I'm a software developer with interests in AI, design, and system building. I love to explore creative tools and technologies.
+      </p>
+    </section>
+  );
+}
