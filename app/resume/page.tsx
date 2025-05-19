8a164faa322560ced8776f29618c9f0aeddaac2c
Rugved Joshi <rugvedsamruddhi@gmail.com>
2025-05-19 20:40:32 +0530

    resume page to display and dwonload resume

---
+++ app/resume/page.tsx
@@ -0,0 +1,10 @@
+export default function Resume() {
+  return (
+    <section className="section container">
+      <h2>Resume</h2>
+      <p>
+        Download my resume <a className="link" href="/resume.pdf" download>here</a>.
+      </p>
+    </section>
+  );
+}
