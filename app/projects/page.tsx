74e5fbdaedaab48ab37c5541d91d42edc56ade18
Rugved Joshi <rugvedsamruddhi@gmail.com>
2025-05-24 20:08:47 +0530

    added new projects and links for github

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,8 +5,10 @@ export default function Projects() {
     <section className="section container">
       <h2>Projects</h2>
       <div className="project-grid">
-        <ProjectCard title="Auralis" description="Python audio processing library." />
-        <ProjectCard title="Eleven" description="Personal AI assistant inspired by Jarvis." />
+        <ProjectCard title="Auralis" description="Python library for speech synthesis and voice cloning" github="https://github.com/joshirugved11/auralis" />
+        <ProjectCard title="LinkedIn-Resume-Bot" description="A bot created to automate the jobs applying processes" github='https://github.com/joshirugved11/LinkedinResumeBot'/>
+        <ProjectCard title="Research-Agent" description="A agent which extracts, summarises and compiles research papers into PDF" github='https://github.com/joshirugved11/Research_Agent'/>
+        <ProjectCard title="Gene-Expression-Prediction" description="Predicting Gene Expressions using SNRNA dataset" github='https://github.com/joshirugved11/Gene-Expression-Project'/>
       </div>
     </section>
   );
